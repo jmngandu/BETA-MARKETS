eb8eb20690b40ae7cb2daf5400148fd3ad50fc89
jmngandu <jamesmngandu@gmail.com>
2024-03-13 10:27:12 +0300

    commit

---
+++ home/src/section/login.js
@@ -0,0 +1,9 @@
+const loginEL = document.querySelector(".loginEL")
+const signEL = document.getElementById("signEL")
+
+loginEL.addEventListener("click", () => {
+    alert("logged in successfully")
+})
+signEL.addEventListener("click", () => {
+    alert("account created")
+})
--- home/src/section/section.js
+++ home/src/section/section.js
@@ -2,3 +2,19 @@ let running = document.querySelector("#starting");
 window.addEventListener("load", function () {
     running.style.display = "none"
 })
+
+const welcomeEl = document.getElementById("signup")
+const loginEL = document.querySelector(".loginEl")
+const signEL = document.getElementById("sign-btn")
+
+welcomeEl.addEventListener("click", () => {
+    alert("welcome to BETAMARKETS")
+})
+loginEL.addEventListener("click", () => {
+    alert("logged in successfully")
+})
+
+
+signEL.addEventListener("click", () => {
+    alert("account created")
+})
